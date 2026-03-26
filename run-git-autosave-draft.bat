9a49fdea101fee6e7b1899671eda079aa0c497fe
giwoong-kim <giung0408@gmail.com>
2026-03-26 18:57:37 +0900

    autosave: 2026-03-26 18:57:36

--- run-git-autosave-draft.bat
+++ run-git-autosave-draft.bat
@@ -1,2 +1,4 @@
 @echo off
-"%ProgramFiles%\Git\bin\bash.exe" "%~dp0git-autosave-draft.sh"
+setlocal
+for /f "delims=" %%I in ('wsl.exe wslpath -a "%~dp0git-autosave-draft.sh"') do set "WSL_SCRIPT=%%I"
+wsl.exe bash "%WSL_SCRIPT%"
