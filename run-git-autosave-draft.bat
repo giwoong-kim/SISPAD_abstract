9534e4b3f64ca73df3610926c00d3914c16bdab4
Kim <giung@BOOK-ATA7M6FVMS.localdomain>
2026-03-26 18:14:08 +0900

    test

--- run-git-autosave-draft.bat
+++ run-git-autosave-draft.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files\Git\bin\bash.exe" "/mnt/c/Users/giung/OneDrive/바탕 화면/PhysAgent/SISPAD 2026/SISPAD_Template_Latex_2026/git-autosave-draft.sh"
+"%ProgramFiles%\Git\bin\bash.exe" "%~dp0git-autosave-draft.sh"
